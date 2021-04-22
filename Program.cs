a8578b45181228f57340e7261e0823fdb43dae1b
Denisbod <Bodartdenis@yahoo.fr>
2021-04-22 11:27:40 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ProductionsMasterMind
+{
+    [Flags]
+    public enum ColorsList {DarkYellow, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow };
+    // les 8 constantes de cette énumération sont écrites comme celles de ConsoleColor Enum.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu au Mastermind, trouvez les 4 couleurs dans le bon ordre\n");
+            char key = default;
+            do
+            {
+                Console.WriteLine("\nSouhaitez vous quitter (touche 'q') ou bien (re)jouer (une autre touche...) ?");
+                if (Console.ReadKey().KeyChar != 'q')
+                {
+                    bool win = false;
+                    int attempt = 0; 
+                    List<String> CoCo;
+
+                    Mastermind Co;
+                    Co = default;
+                    Mastermind Jo;
+                    Jo = default;
+
+                    Co.ComputerColors(out CoCo);
+
+                    do
+                    {
+                        List<String> JoJo;
+                        int count = 0;
+                        attempt++;
+
+                        Jo.JoueurColors(out JoJo);
+
+                        for (int i = 0; i < JoJo.Count; i++)
+                        {
+                            string color = JoJo[i];
+                            Console.ForegroundColor = Enum.Parse<ConsoleColor>(color);
+                            Console.WriteLine($"Case{i} = {color}.");
+                        }
+                        Console.WriteLine("\n");
+
+                        for (int i = 0; i < JoJo.Count; i++)
+                        {
+                            string joColor = JoJo[i];
+                            for (int j = 0; j < CoCo.Count; j++)
+                            {
+                                string coColor = CoCo[j];
+                                if ((JoJo[i] == CoCo[j]) && (i == j))
+                                {
+                                    Console.ForegroundColor = Enum.Parse<ConsoleColor>(joColor);
+                                    Console.WriteLine($"Case{i} = {joColor} est à la bonne place.");
+                                    count++;
+                                }
+                                else if ((JoJo[i] == CoCo[j]) && (i != j))
+                                {
+                                    Console.ForegroundColor = Enum.Parse<ConsoleColor>(joColor);
+                                    Console.WriteLine($"Case{i} = {joColor} n'est pas à la bonne place.");
+                                }
+                            }
+                        }
+
+                        if (count == 4)
+                        {
+                            Console.WriteLine($"\nFelicitations! Vous avez trouvé la bonne combinaison en {attempt} tentatives!");
+                            win = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\nAppuyez sur 'q' pour abandonner ou une autre touche pour poursuivre la partie et choisir une nouvelle combinaison!");
+                            if (Console.ReadKey().KeyChar == 'q')
+                            {
+                                Console.WriteLine("\n\nLa solution était :");
+                                for (int i = 0; i < CoCo.Count; i++)
+                                {
+                                    string color = CoCo[i];
+                                    Console.ForegroundColor = Enum.Parse<ConsoleColor>(color);
+                                    Console.WriteLine($"Case{i} = {color}.");
+                                }
+                                Console.WriteLine("\n");
+                                win = true;
+                            }
+                        }
+                    } while (!win);
+                }
+                else key = 'q';
+            }
+            while (key != 'q');
+            Console.Write($"\n\nAurevoir !\n\n");
+            
+
+        }
+    }
+}
---
+++ Struct.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionsMasterMind
+{
+    public struct Mastermind
+    {
+
+        //public ColorsList Color;
+        
+        #region ComputerColors
+        // Cette méthode gère la sélection de 4 couleurs aléatoires parmis les 8 de l'énumération.
+        // Elle utilise une Arrayliste que l'on ampute des numéros déjà sortis pour simuler un dé.
+        // La seconde liste permet de stocker la couleur correspondant au numéro dans l'énumération.
+        public void ComputerColors(out List<string> Coco)
+        {
+            
+            List<String> RandomColors = new List<string>();
+            Random dice = new Random();
+            ArrayList tab = new ArrayList { 0, 1, 2, 3, 4, 5, 6, 7 };
+            int i = 0;
+            do
+            {
+                int chiffre = dice.Next(tab.Count);
+
+                foreach (var c in Enum.GetNames(typeof(ColorsList)))
+                {
+                    if ((int)tab[chiffre] == (int)Enum.Parse<ColorsList>(c))
+                    {
+                        RandomColors.Add(c);
+                    }
+                }
+                tab.Remove(tab[chiffre]);
+                i++;
+
+            } while (i < 4);
+            Coco = RandomColors;
+        }
+        #endregion
+
+        #region JoueurColors
+        // Cette méthode gère la sélection de 4 couleurs par le joueur.
+        // Elle exploite 2 listes.
+        // (Copie de l'énumération),la première, présentant les couleurs disponibles, est amputée des choix du joueur.
+        // La seconde emmagasine les choix du joueur et leur ordre.
+        public void JoueurColors(out List<string> JoJo)
+        {
+            List<String> ListedColors = new List<string>();
+            List<String> ChoiceColors = new List<string>();;
+            bool b = false;
+            string choice;
+            foreach (string color in Enum.GetNames(typeof(ColorsList)))
+            {
+                Console.WriteLine($"{color}");
+                ListedColors.Add(color);
+            }
+            int i = 0;
+            do
+            {
+                do
+                {
+                Console.Clear();
+                    foreach (string color in ListedColors)
+                    {
+                        Console.WriteLine($"{color}");
+                    }
+                    Console.WriteLine();
+                    foreach (string color in ChoiceColors)
+                    {
+                        int j = 0;
+                        Console.ForegroundColor = Enum.Parse<ConsoleColor>(color);
+                        Console.WriteLine($"Case{i} = {color}.");
+                        j++;
+                    }
+                    Console.Write($"\nChoix de couleur pour la case {i}, Ecrivez l'une de celles-ci en toute lettres (! Majuscule) : ");
+                
+                    choice = Console.ReadLine().ToString();
+                    b = ListedColors.Contains(choice);
+                    if (b) ChoiceColors.Add(choice);
+                }
+                while (!b);
+                i++;
+                ListedColors.Remove(choice);
+            } while (i<4);
+            Console.Clear();
+            JoJo = ChoiceColors;
+        }
+        #endregion
+
+    }
+}
